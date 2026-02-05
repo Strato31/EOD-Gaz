2c2375764cb47636acbd3b91284e05bf5af117ad
Strato31 <chevauxquentin@gmail.com>
2026-02-05 16:48:59 +0100

    ok

--- Optim_mix.jl
+++ Optim_mix.jl
@@ -322,13 +322,8 @@ end
 # @constraint(model, inj_max_CH4_S[t in 1:Tmax], P_inj_CH4_S[t] <= cap_CH4_inj_max)
 # @constraint(model, sout_max_CH4_S[t in 1:Tmax], P_sout_CH4_S[t] <= cap_CH4_sout_max)
 @constraint(model, init_stock_CH4_S, stock_CH4_S[1] == 0)
-<<<<<<< HEAD
-@constraint(model, high_lev_stock_S[t in 1:Tmax], stock_CH4_S[t] <= high_lev_CH4[t]*stock_CH4_max_S)
-@constraint(model, low_lev_stock_S[t in 1:Tmax], low_lev_CH4[t]*stock_CH4_max_S <= stock_CH4_S[t])
-=======
 @constraint(model, stock_CH4_S_high[t in 1:Tmax], stock_CH4_S[t] <= high_lev_CH4[t]*stock_CH4_max_S + slack_CH4_S_high[t])
 @constraint(model, stock_CH4_S_low[t in 1:Tmax], low_lev_CH4[t]*stock_CH4_max_S - slack_CH4_S_low[t] <= stock_CH4_S[t])
->>>>>>> 7d8d24c2b6624d73fa869b22b4179b4752e19b47
 
 #@constraint(model, end_stock_CH4_S, stock_CH4_S[Tmax] == stock_CH4_S[1])
 
@@ -337,13 +332,8 @@ end
 @constraint(model, inj_max_CH4_N[t in 1:Tmax], P_inj_CH4_N[t] <= cap_CH4_inj_max)
 @constraint(model, sout_max_CH4_N[t in 1:Tmax], P_sout_CH4_N[t] <= cap_CH4_sout_max)
 @constraint(model, init_stock_CH4_N, stock_CH4_N[1] == 0)
-<<<<<<< HEAD
-@constraint(model, high_lev_stock_N[t in 1:Tmax],  stock_CH4_N[t] <= high_lev_CH4[t]*stock_CH4_max_N)
-@constraint(model, low_lev_stock_N[t in 1:Tmax], low_lev_CH4[t]*stock_CH4_max_N <= stock_CH4_N[t] )
-=======
 @constraint(model, stock_CH4_N_high[t in 1:Tmax], stock_CH4_N[t] <= high_lev_CH4[t]*stock_CH4_max_N + slack_CH4_N_high[t])
 @constraint(model, stock_CH4_N_low[t in 1:Tmax], low_lev_CH4[t]*stock_CH4_max_N - slack_CH4_N_low[t] <= stock_CH4_N[t])
->>>>>>> 7d8d24c2b6624d73fa869b22b4179b4752e19b47
 
 #@constraint(model, end_stock_CH4_N, stock_CH4_N[Tmax] == stock_CH4_N[1])
 
